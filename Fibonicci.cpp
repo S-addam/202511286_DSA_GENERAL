cb9edbd11717e2a9e76013be47854a65c79c2522
Saddam Khalil <saddamkhalil.211@gmail.com>
2026-05-14 13:23:27 +0200

    Add files via upload

---
+++ Fibonicci.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int fibonacci(int n)
+{
+    if (n == 0)
+        return 0;
+    else if (n == 1)
+        return 1;
+    else
+        return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main()
+{
+    int terms;
+
+    cout << "Enter number of terms: ";
+    cin >> terms;
+
+    for (int i = 0; i < terms; i++)
+    {
+        cout << fibonacci(i) << " ";
+    }
+
+    return 0;
+}
